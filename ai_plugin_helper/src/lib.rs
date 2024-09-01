78ac03e4c794fb454d047b34cf3ffa4bc68e8083
thedtvn <duongtuan30306@gmail.com>
2024-09-01 14:15:11 +0700

    update temp add plugin helper

---
+++ ai_plugin_helper/src/lib.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code)]
+use std::collections::HashMap;
+
+use regex::Regex;
+use serde_json::Value;
+
+#[derive(Debug)]
+struct PluginHelper {
+    name: String,
+    id: String,
+    callback: Option<fn (id: &str, name: &str, kargs: HashMap<&str, Value>) -> Value>,
+}
+
+impl PluginHelper {
+    pub fn new(name: &str, id: &str) -> Result<Self, &'static str> {
+        let re = Regex::new(r"^[a-z0-9_]+$").unwrap();
+        if !re.is_match(&id) {
+            return Err("Invalid ID");
+        }
+        Ok(Self { name: name.to_owned(), id: id.to_owned(), callback: None })
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn get_id(&self) -> String {
+        self.id.clone()
+    }
+
+    pub fn _set_callback(&self, callback: fn (id: &str, name: &str, kargs: HashMap<&str, Value>) -> Value) {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::PluginHelper;
+
+    #[test]
+    fn it_works() {
+        let a = PluginHelper::new("Google", "googe_12321", Some("this is a description".to_string()));
+        println!("{:?}", a);
+        assert!(a.is_ok());
+    }
+}
